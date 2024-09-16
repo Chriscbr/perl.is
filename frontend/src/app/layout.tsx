d3d432d5aecb98d24af56a7d76ccbb4cdafeb605
Christopher Rybicki <crybicki98@gmail.com>
2024-09-15 20:11:09 -0400

    fix scripts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Alan Perlis Quotes",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-HDKF7WRHPR" />
     </html>
   );
 }
